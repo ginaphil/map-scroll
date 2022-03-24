cb0028fd6baa1ac0a1c9f9fa8eeced94944dc7a5
g-na <32394598+ginaphil@users.noreply.github.com>
2022-03-25 00:21:40 +0530

    map

--- src/script/app.js
+++ src/script/app.js
@@ -4,4 +4,4 @@ import enterViewScrolly from "../scrolly/scrolly"
 import * as bootstrap from 'bootstrap';
 console.log('Hello from the console world!');
 
-enterViewScrolly("#doge-scrolly");
+enterViewScrolly("#map-scrolly");
